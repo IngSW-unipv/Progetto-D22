e6d124b87447ce78d79d67a4039708fa05ae1658
Andrea Frigatti <andrea.frigatti01@universitadipavia.it>
2022-02-24 21:50:25 +0100

    HomePage GUI v1.0

---
+++ src/main/java/it/unipv/po/aioobe/trenissimo/HomePage.java
@@ -0,0 +1,24 @@
+package it.unipv.po.aioobe.trenissimo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomePage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("homePage-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 800);
+        stage.setResizable(false);
+        stage.setTitle("Trenissimo");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
