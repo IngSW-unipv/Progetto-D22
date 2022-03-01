a131e9f2331e2b3795ca820e1c593eefa92352a8
Andrea Frigatti <andrea.frigatti01@universitadipavia.it>
2022-03-01 15:40:10 +0100

    Add features to choose trip departure time

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,14 +7,15 @@ module it.unipv.po.aioobe.trenissimo {
     requires java.naming;
     requires java.logging;
     requires org.controlsfx.controls;
+    requires com.jfoenix;
 
 
     opens it.unipv.po.aioobe.trenissimo.model.persistence.entity to org.hibernate.orm.core, javafx.fxml;
     opens it.unipv.po.aioobe.trenissimo to javafx.fxml;
 
-    exports it.unipv.po.aioobe.trenissimo;
     exports it.unipv.po.aioobe.trenissimo.controller;
     opens it.unipv.po.aioobe.trenissimo.controller to javafx.fxml;
     exports it.unipv.po.aioobe.trenissimo.view;
     opens it.unipv.po.aioobe.trenissimo.view to javafx.fxml;
+
 }
