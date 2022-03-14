15b3893a2a69cdbb0c499f72d4a3b48af3eae0b8
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 13:58:01 +0100

    Add Doc Comments to HomePage

--- src/main/java/it/unipv/po/aioobe/trenissimo/view/HomePage.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/view/HomePage.java
@@ -1,7 +1,6 @@
 package it.unipv.po.aioobe.trenissimo.view;
 
-import it.unipv.po.aioobe.trenissimo.controller.RicercaController;
-import it.unipv.po.aioobe.trenissimo.model.viaggio.Viaggio;
+import it.unipv.po.aioobe.trenissimo.controller.AcquistoVoucherController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,30 +8,39 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 
-public class HomePage extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.WARNING);
-        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("homePage/homePage-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1440, 800);
-        stage.setResizable(false);
-        stage.setTitle("Trenissimo");
-        Image img = new Image(HomePage.class.getResourceAsStream("homePage/LogoIcona.png"));
-        stage.getIcons().add(img);
-        stage.setScene(scene);
-        stage.show();
-    }
+/**
+ * Main class che gestisce il render del file homePage-view.fxml
+ *
+ * @see Application
+ * @see it.unipv.po.aioobe.trenissimo.view.acquistoVoucher
+ */
 
+public class HomePage extends Application {
+
+    /**
+     * Lancia il render della parte grafica
+     *
+     * @param args argomenti da linea di comando
+     * @see #launch(String...)
+     */
     public static void main(String[] args) {
         launch();
     }
 
-    public static void openScene(Window owner){
+    /**
+     * Risponde alle chiamate esterne di altri componenti
+     *
+     * @param owner finestra che contiene l'elemento che ha chiamato il metodo
+     * @see HomePage
+     * @see AcquistoVoucherController
+     */
+    public static void openScene(Window owner) {
         FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("homePage/homePage-view.fxml"));
 
         Parent scene = null;
@@ -43,5 +51,27 @@ public class HomePage extends Application {
         }
 
         owner.getScene().setRoot(scene);
+    }
+
+    /**
+     * Risponde alla chiamata del main
+     *
+     * @param stage stage che contiene l'elemento che ha chiamato il metodo
+     * @throws IOException
+     * @see #launch(String...)
+     * @see AccountSettings
+     * @see Application
+     */
+    @Override
+    public void start(@NotNull Stage stage) throws IOException {
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.WARNING);
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("homePage/homePage-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 800);
+        stage.setResizable(false);
+        stage.setTitle("Trenissimo");
+        Image img = new Image(Objects.requireNonNull(HomePage.class.getResourceAsStream("homePage/LogoIcona.png")));
+        stage.getIcons().add(img);
+        stage.setScene(scene);
+        stage.show();
     }
 }
