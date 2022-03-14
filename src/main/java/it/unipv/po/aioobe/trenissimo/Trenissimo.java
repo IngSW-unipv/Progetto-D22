77a7c702d8402ddcc407fe9b9f33b8248fdb5cdf
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 14:28:36 +0100

    Add Doc Comments to Trenissimo

--- src/main/java/it/unipv/po/aioobe/trenissimo/Trenissimo.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/Trenissimo.java
@@ -4,12 +4,26 @@ import it.unipv.po.aioobe.trenissimo.view.Splashscreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * Entrypoint
+ */
 public class Trenissimo extends Application {
 
+    /**
+     * Main entrypoint
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         launch();
     }
 
+    /**
+     * Lancia lo Splashscreen
+     *
+     * @param stage
+     * @throws Exception
+     */
     @Override
     public void start(Stage stage) throws Exception {
         new Splashscreen().start(stage);
