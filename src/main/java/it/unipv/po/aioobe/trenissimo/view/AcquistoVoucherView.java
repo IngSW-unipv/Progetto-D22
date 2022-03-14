b661a7245b1369aed4e7fad070838bd8e3f3bc21
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 13:42:15 +0100

    Add Doc Comments to AcquistoVoucherView

--- src/main/java/it/unipv/po/aioobe/trenissimo/view/AcquistoVoucherView.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/view/AcquistoVoucherView.java
@@ -1,5 +1,6 @@
 package it.unipv.po.aioobe.trenissimo.view;
 
+import it.unipv.po.aioobe.trenissimo.controller.AccountSettingsController;
 import it.unipv.po.aioobe.trenissimo.controller.AcquistoVoucherController;
 import it.unipv.po.aioobe.trenissimo.model.titolodiviaggio.enumeration.ValoreVoucher;
 import javafx.application.Application;
@@ -9,24 +10,36 @@ import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
+/**
+ * Main class che gestisce il render del file acquistoVoucher.fxml
+ *
+ * @see Application
+ * @see it.unipv.po.aioobe.trenissimo.view.acquistoVoucher
+ */
 public class AcquistoVoucherView extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("acquistoVoucher/acquistoVoucher.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("AcquistoVoucher");
-        stage.setScene(scene);
-        stage.show();
-    }
 
+    /**
+     * Lancia il render della parte grafica
+     *
+     * @param args argomenti da linea di comando
+     * @see #launch(String...)
+     */
     public static void main(String[] args) {
         launch();
     }
 
-    public static void openWindow(Window owner){
+    /**
+     * Risponde alle chiamate esterne di altri componenti
+     *
+     * @param owner finestra che contiene l'elemento che ha chiamato il metodo
+     * @see Login
+     * @see AccountSettingsController
+     */
+    public static void openWindow(Window owner) {
         FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("acquistoVoucher/acquistoVoucher.fxml"));
         Stage stage = new Stage();
         stage.initModality(Modality.WINDOW_MODAL);
@@ -42,7 +55,16 @@ public class AcquistoVoucherView extends Application {
         stage.show();
     }
 
-    public static void openScene(Window owner, ValoreVoucher importo){
+    /**
+     * Risponde alle chiamate esterne di altri componenti
+     *
+     * @param owner   finestra che contiene l'elemento che ha chiamato il metodo
+     * @param importo lista di viaggi da visualizzare
+     * @see ValoreVoucher
+     * @see HomePage
+     * @see AcquistoVoucherController
+     */
+    public static void openScene(Window owner, ValoreVoucher importo) {
         FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("acquistoVoucher/acquistoVoucher.fxml"));
         Parent scene = null;
         try {
@@ -52,5 +74,23 @@ public class AcquistoVoucherView extends Application {
         }
         ((AcquistoVoucherController) fxmlLoader.getController()).setVoucher(importo);
         owner.getScene().setRoot(scene);
+    }
+
+    /**
+     * Risponde alla chiamata del main
+     *
+     * @param stage stage che contiene l'elemento che ha chiamato il metodo
+     * @throws IOException
+     * @see #launch(String...)
+     * @see AccountSettings
+     * @see Application
+     */
+    @Override
+    public void start(@NotNull Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("acquistoVoucher/acquistoVoucher.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("AcquistoVoucher");
+        stage.setScene(scene);
+        stage.show();
     }
 }
