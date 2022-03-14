182c754827b1791928a89784df28c0d794ab668f
martinimatteo10 <matteo.martini02@universitadipavia.it>
2022-03-14 12:11:19 +0100

    Update JavaDoc: prezzoTotCambi

--- src/main/java/it/unipv/po/aioobe/trenissimo/model/viaggio/utils/strategy/prezzoTotCambi/PrezzoTotCambiFactory.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/model/viaggio/utils/strategy/prezzoTotCambi/PrezzoTotCambiFactory.java
@@ -1,7 +1,5 @@
 package it.unipv.po.aioobe.trenissimo.model.viaggio.utils.strategy.prezzoTotCambi;
 
-import it.unipv.po.aioobe.trenissimo.model.viaggio.utils.strategy.prezzoPerDistanza.IPrezzoPerDistanzaStrategy;
-
 import java.io.FileInputStream;
 import java.lang.reflect.Constructor;
 import java.util.Properties;
