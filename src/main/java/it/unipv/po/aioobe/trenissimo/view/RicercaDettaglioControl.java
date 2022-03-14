7e71ceaed0548a00f33bbdd5d6a3d2f0679f3bc7
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 15:54:43 +0100

    Add Doc Comments to RicercaDettaglioControl

--- src/main/java/it/unipv/po/aioobe/trenissimo/view/RicercaDettaglioControl.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/view/RicercaDettaglioControl.java
@@ -7,7 +7,6 @@ import it.unipv.po.aioobe.trenissimo.model.viaggio.filtri.FiltroOrario;
 import it.unipv.po.aioobe.trenissimo.model.viaggio.filtri.FiltroOrdina;
 import it.unipv.po.aioobe.trenissimo.model.viaggio.filtri.FiltroPrezzo;
 import it.unipv.po.aioobe.trenissimo.model.viaggio.filtri.IFiltro;
-import it.unipv.po.aioobe.trenissimo.model.viaggio.ricerca.Ricerca;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -19,6 +18,7 @@ import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 import org.controlsfx.control.RangeSlider;
 import org.controlsfx.control.SegmentedButton;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.time.LocalTime;
@@ -27,22 +27,43 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Main class che gestisce il render del file registrazione-view.fxml
+ *
+ * @see BorderPane
+ * @see Viaggio
+ * @see IFiltro
+ * @see FiltroOrdina
+ * @see it.unipv.po.aioobe.trenissimo.view.acquistoVoucher
+ */
 public class RicercaDettaglioControl extends BorderPane {
-    @FXML private VBox boxAndata;
+    @FXML
+    private VBox boxAndata;
 
-    @FXML private RangeSlider rngPrezzo;
-    @FXML private JFXTimePicker tmpPartenza;
-    @FXML private JFXTimePicker tmpArrivo;
-    @FXML private SegmentedButton sbtFiltro;
+    @FXML
+    private RangeSlider rngPrezzo;
+    @FXML
+    private JFXTimePicker tmpPartenza;
+    @FXML
+    private JFXTimePicker tmpArrivo;
+    @FXML
+    private SegmentedButton sbtFiltro;
 
-    private ObservableList<Viaggio> viaggi;
-    private ObservableList<IFiltro> filtri;
+    private final ObservableList<Viaggio> viaggi;
+    private final ObservableList<IFiltro> filtri;
 
-    private Callback<Viaggio,Void> onAddToCart;
+    private Callback<Viaggio, Void> onAddToCart;
 
 
     private FiltroOrdina.Criterio criterioOrdine;
 
+    /**
+     * Controller per la visualizzazione dei risultati
+     *
+     * @see FiltroOrdina
+     * @see Viaggio
+     * @see IFiltro
+     */
     public RicercaDettaglioControl() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("RicercaDettaglioControl/ricercaDettaglioControl.fxml"));
         fxmlLoader.setRoot(this);
@@ -58,17 +79,25 @@ public class RicercaDettaglioControl extends BorderPane {
 
         filtri = FXCollections.observableArrayList();
 
-        rngPrezzo.highValueChangingProperty()   .addListener((obs, old, newV) -> { if (!newV) { updateFiltri(); }});
-        rngPrezzo.lowValueChangingProperty()    .addListener((obs, old, newV) -> { if (!newV) { updateFiltri(); }});
+        rngPrezzo.highValueChangingProperty().addListener((obs, old, newV) -> {
+            if (!newV) {
+                updateFiltri();
+            }
+        });
+        rngPrezzo.lowValueChangingProperty().addListener((obs, old, newV) -> {
+            if (!newV) {
+                updateFiltri();
+            }
+        });
 
-        tmpPartenza     .valueProperty().addListener((obs, old, newV) -> updateFiltri());
-        tmpArrivo       .valueProperty().addListener((obs, old, newV) -> updateFiltri());
+        tmpPartenza.valueProperty().addListener((obs, old, newV) -> updateFiltri());
+        tmpArrivo.valueProperty().addListener((obs, old, newV) -> updateFiltri());
 
-        tmpPartenza     .set24HourView(true);
-        tmpArrivo       .set24HourView(true);
+        tmpPartenza.set24HourView(true);
+        tmpArrivo.set24HourView(true);
 
-        tmpPartenza     .setValue(LocalTime.MIN);
-        tmpArrivo       .setValue(LocalTime.MAX);
+        tmpPartenza.setValue(LocalTime.MIN);
+        tmpArrivo.setValue(LocalTime.MAX);
 
         viaggi.addListener((ListChangeListener<Viaggio>) c -> updateList());
         filtri.addListener((ListChangeListener<IFiltro>) c -> updateList());
@@ -82,15 +111,29 @@ public class RicercaDettaglioControl extends BorderPane {
     }
 
 
-    private void onOrdina(){
-        if (sbtFiltro.getButtons().stream().filter(ToggleButton::isSelected).findAny().isEmpty()){
+    /**
+     * Ordina i viaggi visualizzati
+     *
+     * @see FiltroOrdina
+     */
+    private void onOrdina() {
+        if (sbtFiltro.getButtons().stream().filter(ToggleButton::isSelected).findAny().isEmpty()) {
             sbtFiltro.getButtons().stream().findFirst().get().setSelected(true);
         }
         criterioOrdine = (FiltroOrdina.Criterio) sbtFiltro.getButtons().stream().filter(ToggleButton::isSelected).findAny().get().getUserData();
         updateFiltri();
     }
 
-    private List<Viaggio> filtra(List<Viaggio> viaggi){
+    /**
+     * Applica alla lista di viaggi i filtri all'interno della lista filtri
+     *
+     * @param viaggi lista di viaggi da ordinare
+     * @return lista di viaggi filtrati
+     * @see #filtri
+     * @see Viaggio
+     * @see IFiltro
+     */
+    private List<Viaggio> filtra(List<Viaggio> viaggi) {
         List<Viaggio> viaggiFiltrati = viaggi;
         for (IFiltro filtro : filtri) {
             viaggiFiltrati = filtro.esegui(viaggiFiltrati);
@@ -98,25 +141,47 @@ public class RicercaDettaglioControl extends BorderPane {
         return viaggiFiltrati;
     }
 
-    private void updateList(){
+    /**
+     * Aggiorna la lista dei viaggi mostrati
+     */
+    private void updateList() {
         boxAndata.getChildren().setAll(filtra(viaggi).stream().map(x -> new ViaggioControl(x, param -> {
             onAddToCart.call(x);
             return null;
         })).toList());
     }
 
-    private void updateFiltri(){
+    /**
+     * Aggiorna la lista dei filtri nel caso presenti altrimenti applica quelli di default
+     * (per data di partenza crescente)
+     *
+     * @see IFiltro
+     * @see FiltroPrezzo
+     * @see FiltroOrdina
+     * @see FiltroOrario
+     */
+    private void updateFiltri() {
         List<IFiltro> newFiltri = new ArrayList<>();
-        newFiltri.add(new FiltroPrezzo(rngPrezzo.getLowValue(),rngPrezzo.getHighValue()));
+        newFiltri.add(new FiltroPrezzo(rngPrezzo.getLowValue(), rngPrezzo.getHighValue()));
         if (criterioOrdine != null) newFiltri.add(new FiltroOrdina(criterioOrdine));
-        if (tmpPartenza.getValue() != null && tmpArrivo.getValue() != null) newFiltri.add(new FiltroOrario(tmpPartenza.getValue().toSecondOfDay(),tmpArrivo.getValue().toSecondOfDay()));
+        if (tmpPartenza.getValue() != null && tmpArrivo.getValue() != null)
+            newFiltri.add(new FiltroOrario(tmpPartenza.getValue().toSecondOfDay(), tmpArrivo.getValue().toSecondOfDay()));
         filtri.setAll(newFiltri);
     }
 
-    public void setViaggi(List<Viaggio> viaggi, LocalTime oraPartenza, Callback<Viaggio,Void> onAddToCart) {
+    /**
+     * Setta i viaggi visualizzabili e i limiti dello slider prezzo
+     *
+     * @param viaggi      lista di viaggi da mostrare
+     * @param oraPartenza orario dal quale si vogliono iniziare a vedere i viaggi
+     * @param onAddToCart metodo per aggiungere il viaggio al carrello
+     * @see #rngPrezzo
+     * @see #updateFiltri()
+     */
+    public void setViaggi(@NotNull List<Viaggio> viaggi, LocalTime oraPartenza, Callback<Viaggio, Void> onAddToCart) {
         this.onAddToCart = onAddToCart;
-        rngPrezzo.setMax(Utils.ceil     (viaggi.stream().max(Comparator.comparing(Viaggio::getPrezzoTot)).get().getPrezzoTot(),-1));
-        rngPrezzo.setMin(Utils.floor    (viaggi.stream().min(Comparator.comparing(Viaggio::getPrezzoTot)).get().getPrezzoTot(),-1));
+        rngPrezzo.setMax(Utils.ceil(viaggi.stream().max(Comparator.comparing(Viaggio::getPrezzoTot)).get().getPrezzoTot(), -1));
+        rngPrezzo.setMin(Utils.floor(viaggi.stream().min(Comparator.comparing(Viaggio::getPrezzoTot)).get().getPrezzoTot(), -1));
         rngPrezzo.setLowValue(rngPrezzo.getMin());
         rngPrezzo.setHighValue(rngPrezzo.getMax());
         tmpPartenza.setValue(oraPartenza);
