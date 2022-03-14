30f123e2b7793266038ad8cfad93b8769fe9607b
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 13:36:00 +0100

    Add Doc Comments to AccountSettings

--- src/main/java/it/unipv/po/aioobe/trenissimo/view/AccountSettings.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/view/AccountSettings.java
@@ -1,53 +1,42 @@
 package it.unipv.po.aioobe.trenissimo.view;
 
 import it.unipv.po.aioobe.trenissimo.controller.AccountSettingsController;
-import it.unipv.po.aioobe.trenissimo.controller.RicercaController;
-import it.unipv.po.aioobe.trenissimo.model.viaggio.Viaggio;
-import it.unipv.po.aioobe.trenissimo.model.viaggio.ricerca.Ricerca;
-import it.unipv.po.aioobe.trenissimo.model.viaggio.ricerca.utils.Connection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Main class che gestisce il render del file accountSettings-view.fxml
+ *
+ * @see Application
+ * @see it.unipv.po.aioobe.trenissimo.view.accountSettingsView
+ */
 public class AccountSettings extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(AccountSettings.class.getResource("accountSettingsView/accountSettings-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1440, 800);
-        stage.setResizable(false);
-        stage.setTitle("Trenissimo");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+    /**
+     * Lancia il render della parte grafica
+     *
+     * @param args argomenti da linea di comando
+     * @see #launch(String...)
+     * @see #start(Stage)
+     */
     public static void main(String[] args) {
         launch();
     }
 
-    public static void open() {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("accountSettingsView/accountSettings-view.fxml"));
-        Scene scene = null;
-        try {
-            scene = new Scene(fxmlLoader.load(), 1440, 800);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Stage stage = new Stage();
-        stage.setResizable(false);
-        stage.setTitle("Login");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void openScene(Window owner){
+    /**
+     * Risponde alle chiamate esterne di altri componenti
+     *
+     * @param owner finestra che contiene l'elemento che ha chiamato il metodo
+     * @see Login
+     * @see AccountSettingsController
+     */
+    public static void openScene(Window owner) {
         FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("accountSettingsView/accountSettings-view.fxml"));
 
         Parent scene = null;
@@ -60,6 +49,25 @@ public class AccountSettings extends Application {
         ((AccountSettingsController) fxmlLoader.getController()).setStoricoAcquisti();
 
         owner.getScene().setRoot(scene);
+    }
+
+    /**
+     * Risponde alla chiamata del main
+     *
+     * @param stage stage che contiene l'elemento che ha chiamato il metodo
+     * @throws IOException
+     * @see #launch(String...)
+     * @see AccountSettings
+     * @see Application
+     */
+    @Override
+    public void start(@NotNull Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AccountSettings.class.getResource("accountSettingsView/accountSettings-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 800);
+        stage.setResizable(false);
+        stage.setTitle("Trenissimo");
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
