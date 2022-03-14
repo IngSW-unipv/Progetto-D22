8123c431426d0ab77f6d5803218a5e4999f2d12f
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 16:20:05 +0100

    Refactor FiltroOrario (method name fix)

--- src/main/java/it/unipv/po/aioobe/trenissimo/model/viaggio/filtri/FiltroOrario.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/model/viaggio/filtri/FiltroOrario.java
@@ -2,11 +2,13 @@ package it.unipv.po.aioobe.trenissimo.model.viaggio.filtri;
 
 
 import it.unipv.po.aioobe.trenissimo.model.viaggio.Viaggio;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 /**
  * Classe che implementa il filtro per orario massimo e minimo
+ *
  * @author ArrayIndexOutOfBoundsException
  */
 public class FiltroOrario implements IFiltro {
@@ -18,28 +20,30 @@ public class FiltroOrario implements IFiltro {
         this.maxOrario = maxOrario;
     }
 
-    public int getMinPrezzo() {
+    public int getMinOrario() {
         return minOrario;
     }
 
-    public void setMinPrezzo(int minPrezzo) {
-        this.minOrario = minPrezzo;
+    public void setMinOrario(int minOrario) {
+        this.minOrario = minOrario;
     }
 
-    public int getMaxPrezzo() {
+    public int getMaxOrario() {
         return maxOrario;
     }
 
-    public void setMaxPrezzo(int maxPrezzo) {
-        this.maxOrario = maxPrezzo;
+    public void setMaxOrario(int maxOrario) {
+        this.maxOrario = maxOrario;
     }
+
     /**
      * Metodo che filtra per orario massimo e minimo la lista di viaggi presa come parametro
+     *
      * @param input lista di Viaggio
      * @return lista di Viaggio
      */
     @Override
-    public List<Viaggio> esegui(List<Viaggio> input) {
+    public List<Viaggio> esegui(@NotNull List<Viaggio> input) {
         return input.stream().filter(x -> x.getOrarioPartenza() >= minOrario && x.getOrarioArrivo() <= maxOrario).toList();
     }
 }
