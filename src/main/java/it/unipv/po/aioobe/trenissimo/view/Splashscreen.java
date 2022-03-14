dfe3e38deec2dce93dc5062661d9622fd0506090
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 15:24:57 +0100

    Add Doc Comments to Splashscreen

--- src/main/java/it/unipv/po/aioobe/trenissimo/view/Splashscreen.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/view/Splashscreen.java
@@ -3,24 +3,47 @@ package it.unipv.po.aioobe.trenissimo.view;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.logging.Level;
 
+/**
+ * Main class che gestisce il render del file splashscreen.fxml
+ *
+ * @see Application
+ * @see it.unipv.po.aioobe.trenissimo.view.splashScreen
+ */
+
 public class Splashscreen extends Application {
 
+    /**
+     * Lancia il render della parte grafica
+     *
+     * @param args argomenti da linea di comando
+     * @see #launch(String...)
+     * @see #start(Stage)
+     */
     public static void main(String[] args) {
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
         launch(args);
     }
 
+    /**
+     * Risponde alla chiamata del main
+     *
+     * @param stage stage che contiene l'elemento che ha chiamato il metodo
+     * @throws IOException
+     * @see #launch(String...)
+     * @see Application
+     * @see Splashscreen
+     */
     @Override
-    public void start(Stage stage) throws IOException {
-        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+    public void start(@NotNull Stage stage) throws IOException {
+        java.util.logging.Logger.getLogger("org.hibernate").setLevel(Level.OFF);
 
         FXMLLoader fxmlLoader = new FXMLLoader(Splashscreen.class.getResource("splashScreen/splashscreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 640, 380);
