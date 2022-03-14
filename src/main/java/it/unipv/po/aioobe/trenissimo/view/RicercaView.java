2e8917f2aeb136a1cafd6c52c2d3bda221897b34
Fabio Zamboni <69969487+zamb0@users.noreply.github.com>
2022-03-14 15:54:58 +0100

    Minor Changes

--- src/main/java/it/unipv/po/aioobe/trenissimo/view/RicercaView.java
+++ src/main/java/it/unipv/po/aioobe/trenissimo/view/RicercaView.java
@@ -1,9 +1,9 @@
 package it.unipv.po.aioobe.trenissimo.view;
 
 import it.unipv.po.aioobe.trenissimo.controller.RicercaController;
+import it.unipv.po.aioobe.trenissimo.model.viaggio.Viaggio;
 import it.unipv.po.aioobe.trenissimo.model.viaggio.ricerca.Ricerca;
 import it.unipv.po.aioobe.trenissimo.model.viaggio.ricerca.utils.Connection;
-import it.unipv.po.aioobe.trenissimo.model.viaggio.Viaggio;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -18,52 +19,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RicercaView extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("ricercaView/ricercaView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
-
-        List<Connection> cambi = new ArrayList<>();
-
-        // Per prototipi più veloci
-
-        cambi.add(new Connection(367, 3053,25500, 25980, 14158963, 14158963));
-        cambi.add(new Connection(3053, 2036,26040, 26340, 14158963, 14158963));
-        cambi.add(new Connection(2036, 1858,26400, 26820, 14158963, 14158963));
-        cambi.add(new Connection(1858, 406, 26880, 27120, 14156467, 14156467));
-
-
-
-
-
-        Viaggio viaggio1 = new Viaggio();
-        viaggio1.setCambi(cambi);
-        viaggio1.setNumAdulti(1);
-
-        Viaggio viaggio2 = new Viaggio();
-        viaggio2.setCambi(cambi);
-        viaggio2.setNumAdulti(2);
-
-        List<Viaggio> viaggi = new ArrayList<>();
-
-        viaggi.add(viaggio2);
-        viaggi.add(viaggio1);
-
-        Ricerca ricerca = new Ricerca(0,0, LocalDateTime.now());
-        ricerca.setRisultatiAndata(viaggi);
-
-        ((RicercaController) fxmlLoader.getController()).setRicerca(ricerca);
-        stage.setTitle("RicercaView");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+    /**
+     * Lancia il render della parte grafica
+     *
+     * @param args argomenti da linea di comando
+     * @see #launch(String...)
+     * @see #start(Stage)
+     */
     public static void main(String[] args) {
         launch();
     }
 
-    public static void openWindow(Ricerca ricerca, Window owner){
+    /**
+     * Risponde alle chiamate esterne di altri componenti
+     *
+     * @param ricerca ricerca che si vuole mostrare a video
+     * @param owner   finestra che contiene l'elemento che ha chiamato il metodo
+     * @see HomePage
+     * @see RicercaController
+     */
+    public static void openWindow(Ricerca ricerca, Window owner) {
         FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("ricercaView/ricercaView.fxml"));
         Stage stage = new Stage();
         stage.initModality(Modality.WINDOW_MODAL);
@@ -80,7 +56,15 @@ public class RicercaView extends Application {
         stage.show();
     }
 
-    public static void openScene(Ricerca ricerca, Window owner){
+    /**
+     * Risponde alle chiamate esterne di altri componenti
+     *
+     * @param ricerca
+     * @param owner   finestra che contiene l'elemento che ha chiamato il metodo
+     * @see HomePage
+     * @see RicercaController
+     */
+    public static void openScene(Ricerca ricerca, Window owner) {
         FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("ricercaView/ricercaView.fxml"));
 
         Parent scene = null;
@@ -92,5 +76,57 @@ public class RicercaView extends Application {
 
         ((RicercaController) fxmlLoader.getController()).setRicerca(ricerca);
         owner.getScene().setRoot(scene);
+    }
+
+    /**
+     * Risponde alla chiamata del main
+     *
+     * @param stage stage che contiene l'elemento che ha chiamato il metodo
+     * @throws IOException
+     * @see #launch(String...)
+     * @see Application
+     * @see HomePage
+     * @see Connection
+     * @see Viaggio
+     * @see Ricerca
+     * @see RicercaController
+     */
+
+    @Override
+    public void start(@NotNull Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePage.class.getResource("ricercaView/ricercaView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+
+        List<Connection> cambi = new ArrayList<>();
+
+        // Per prototipi più veloci
+
+        cambi.add(new Connection(367, 3053, 25500, 25980, 14158963, 14158963));
+        cambi.add(new Connection(3053, 2036, 26040, 26340, 14158963, 14158963));
+        cambi.add(new Connection(2036, 1858, 26400, 26820, 14158963, 14158963));
+        cambi.add(new Connection(1858, 406, 26880, 27120, 14156467, 14156467));
+
+
+        Viaggio viaggio1 = new Viaggio();
+        viaggio1.setCambi(cambi);
+        viaggio1.setNumAdulti(1);
+
+        Viaggio viaggio2 = new Viaggio();
+        viaggio2.setCambi(cambi);
+        viaggio2.setNumAdulti(2);
+
+        List<Viaggio> viaggi = new ArrayList<>();
+
+        viaggi.add(viaggio2);
+        viaggi.add(viaggio1);
+
+        Ricerca ricerca = new Ricerca(0, 0, LocalDateTime.now());
+        ricerca.setRisultatiAndata(viaggi);
+
+        ((RicercaController) fxmlLoader.getController()).setRicerca(ricerca);
+        stage.setTitle("RicercaView");
+        stage.setScene(scene);
+        stage.show();
     }
 }
